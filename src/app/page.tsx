fe1826a2d3602521139032511da5b6761e7d79ac
refusado <refusado@gmail.com>
2024-08-03 06:09:18 -0300

    starting an onboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import { Onboard } from '@/components/onboard';
+
 export default function Home() {
   return (
     <main>
       <section className="container">
-        <h1>Home</h1>
+        <h1>Vamos construir o jardim que combina com você</h1>
+        <Onboard />
       </section>
     </main>
   );
