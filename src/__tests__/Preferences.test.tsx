de4a77dad9af0bdc65a5f95540d090f0281b2e4b
refusado <refusado@gmail.com>
2024-08-04 20:39:02 -0300

    refactor and tests for Preferences component

---
+++ src/__tests__/Preferences.test.tsx
@@ -0,0 +1,79 @@
+// Preferences.test.tsx
+import { render, screen, fireEvent } from '@testing-library/react';
+import { describe, it, expect, vi } from 'vitest';
+import { Preferences } from '@/app/explorar/Preferences';
+import { questions } from '@/utils/questions';
+import { OnboardResponses } from '@/components/onboard';
+
+describe('Preferences Component', () => {
+  it('should render onboarding redirect when there is no filters', () => {
+    render(
+      <Preferences filters={null} clearPreferences={() => {}} userTags={[]} />,
+    );
+
+    expect(screen.getByText('Complete o onboarding aqui')).toBeDefined();
+  });
+
+  it('should display correct user tags based on the selected preferences', () => {
+    const filters: OnboardResponses = {
+      q1: 3,
+      q2: 2,
+      q3: 1,
+      q4: 1,
+      q5: 0,
+    };
+    const expectedTags = [
+      'interno',
+      'luz baixa',
+      'moderada',
+      'médio',
+      'cuidado diário',
+    ];
+
+    let userTags: string[] = [];
+    questions.forEach(({ id, options }) => {
+      const selectedOption = filters[id];
+
+      if (options[selectedOption]) {
+        userTags.push(...options[selectedOption].tags);
+      }
+    });
+
+    expect(userTags).toEqual(expectedTags);
+
+    render(
+      <Preferences
+        filters={filters}
+        userTags={userTags}
+        clearPreferences={() => {}}
+      />,
+    );
+
+    expectedTags.forEach((tag) => {
+      expect(screen.getByText(tag)).toBeDefined();
+    });
+  });
+
+  it('should call clearPreferences function when clear button is clicked', () => {
+    const preferences = {
+      clear() {},
+    };
+
+    const clearPreferencesSpy = vi.spyOn(preferences, 'clear');
+
+    const { container } = render(
+      <Preferences
+        filters={{}}
+        clearPreferences={preferences.clear}
+        userTags={[]}
+      />,
+    );
+
+    const button = container.querySelector('button');
+    expect(button).toBeDefined();
+    expect(button?.innerHTML).toContain('Limpar preferências');
+
+    if (button) fireEvent.click(button);
+    expect(clearPreferencesSpy).toHaveBeenCalledTimes(1);
+  });
+});
--- src/app/explorar/Preferences.tsx
+++ src/app/explorar/Preferences.tsx
@@ -11,37 +11,41 @@ export function Preferences({
   clearPreferences: () => void;
   userTags: string[];
 }) {
-  if (filters)
-    return (
-      <>
-        <p className="mb-3">Suas preferências: </p>
-        <ul className="mb-10 flex flex-wrap gap-2">
-          <li>
-            <button
-              className="flex cursor-pointer items-center gap-1 rounded-full border border-yellow-600/25 bg-yellow-500/15 px-4 py-1 duration-200 hover:border-yellow-600/60 hover:bg-yellow-500/20"
-              onClick={clearPreferences}
-            >
-              <CgClose /> Limpar
-            </button>
-          </li>
-          {userTags.map((tag, i) => (
-            <li
-              key={i}
-              className="rounded-full border border-black/15 bg-black/5 px-4 py-1 duration-200 hover:bg-black/10"
-            >
-              {tag}
-            </li>
-          ))}
-        </ul>
-      </>
-    );
+  if (!filters) return <RedirectToOnboard />;
 
   return (
-    <p className="mb-8">
-      <Link href="/#onboard" className="text-blue-500 hover:underline">
-        Complete o onboarding aqui
-      </Link>{' '}
-      para encontrar as plantas ideias para você
+    <>
+      <p className="mb-3">Suas preferências: </p>
+      <ul className="mb-10 flex flex-wrap gap-2">
+        {userTags.map((tag, i) => (
+          <li
+            key={i}
+            className="rounded-full border border-black/15 bg-black/5 px-4 py-1 duration-200 hover:bg-black/10"
+          >
+            {tag}
+          </li>
+        ))}
+        <li>
+          <button
+            className="flex cursor-pointer items-center gap-1 rounded-full border border-yellow-600/25 bg-yellow-500/15 px-4 py-1 duration-200 hover:border-yellow-600/60 hover:bg-yellow-500/20"
+            onClick={clearPreferences}
+          >
+            <CgClose /> Limpar preferências
+          </button>
+        </li>
+      </ul>
+    </>
+  );
+}
+
+export function RedirectToOnboard() {
+  return (
+    <p className="mb-8 max-w-2xl text-lg">
+      Precisa de ajuda? Saiba exatamente quais são as plantas perfeitas para
+      você{' '}
+      <Link href="/#onboard" className="text-green-700 hover:underline">
+        respondendo este questionário rápido
+      </Link>
     </p>
   );
 }
